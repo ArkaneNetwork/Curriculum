2f6bd5a70e1393263c82162e827c931938e2dbc4
Quinten De Swaef <quintendeswaef@gmail.com>
2019-05-07 16:31:45 +0200

    nodes

--- nodes/send.sh
+++ nodes/send.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 
-curl --data '{"jsonrpc":"2.0","method":"personal_sendTransaction","params":[{"from":"0x6B0c56d1Ad5144b4d37fa6e27DC9afd5C2435c3B","to":"0x00E3d1Aa965aAfd61217635E5f99f7c1e567978f","value":"0xde0b6b3a7640000"}, ""],"id":0}' -H "Content-Type: application/json" -X POST localhost:8545
+curl --data '{"jsonrpc":"2.0","method":"personal_sendTransaction","params":[{"from":"0x6B0c56d1Ad5144b4d37fa6e27DC9afd5C2435c3B","to":"0xE413671FE2A436E282D927D24611ddD529CfE605","value":"0xde0b6b3a7640000"}, ""],"id":0}' -H "Content-Type: application/json" -X POST localhost:8545
