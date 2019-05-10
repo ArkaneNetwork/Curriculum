ec921188af9eef1de811c36fd9d5a1d245c9c411
davyvanroy <davy.van.roy@gmail.com>
2019-05-10 14:03:54 +0200

    vechain stress test

--- VechainJmeterTest/src/main/java/be/eurofins/vechain/vechain/VechainStressTest.java
+++ VechainJmeterTest/src/main/java/be/eurofins/vechain/vechain/VechainStressTest.java
@@ -1,5 +1,6 @@
 package be.eurofins.vechain.vechain;
 
+import network.arkane.provider.secret.generation.VechainSecretKey;
 import org.apache.jmeter.config.Arguments;
 import org.apache.jmeter.protocol.java.sampler.JavaSamplerClient;
 import org.apache.jmeter.protocol.java.sampler.JavaSamplerContext;
@@ -8,19 +9,39 @@ import org.apache.jmeter.samplers.SampleResult;
 public class VechainStressTest implements JavaSamplerClient {
 
     private TransactionService transactionService = new TransactionService();
+    private VechainSecretKey vechainSecretKey;
 
     @Override
     public void setupTest(JavaSamplerContext javaSamplerContext) {
         //load wallet
-
+        String pk = javaSamplerContext.getParameter("pk");
+        vechainSecretKey = transactionService.loadWallet(pk);
     }
 
     @Override
     public SampleResult runTest(JavaSamplerContext javaSamplerContext) {
-        //submit transaction
-        //"wait" for receipt
-        //return successful or error
-        return null;
+        SampleResult result = new SampleResult();
+        result.sampleStart();
+        result.setSampleLabel("Vechain Test Sample");
+        try {
+            SubmissionResult submittedTx = transactionService.executeTransaction(vechainSecretKey);
+            ReceiptResult status = transactionService.getReceipt(submittedTx);
+            while (status == null) {
+                Thread.sleep(100);
+                status = transactionService.getReceipt(submittedTx);
+            }
+            result.sampleEnd();
+            result.setResponseCode("200");
+            result.setResponseMessage(submittedTx.getId());
+            result.setSuccessful(true);
+        } catch (Exception e) {
+            e.printStackTrace();
+            result.sampleEnd();
+            result.setResponseCode("500");
+            result.setResponseMessage(e.getMessage());
+            result.setSuccessful(false);
+        }
+        return result;
     }
 
     @Override
